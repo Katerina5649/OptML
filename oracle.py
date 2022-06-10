b46639f204c790bafc7f33ce82f1d2c89812a8c8
ndelamaide <nicolas.delamaide@gmail.com>
2022-06-10 16:34:03 +0200

    added notebook + oracle class

---
+++ oracle.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Oracle:
+
+    def __init__(self, func_type):
+        
+        self.type = func_type
+        
+    
+    def getOracle(self, x):
+
+        if self.type == "strongly convex":
+            return np.power(x, 2), 2 * x
+        
+    def getMin(self):
+        
+        if self.type == "strongly convex":
+            return 0
+        
+    def __call__(self, x):
+        return self.getOracle(x)
+        
+    
