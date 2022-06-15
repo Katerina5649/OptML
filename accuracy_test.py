added averaging and corrected oracle

accuracy_test.py
@@ -18,11 +18,11 @@ def accuracy_check(function_type, x, oracle, x_star, f_star, mu, W, plot=True):
 #if function is not stronly convex, input -1 for mu
     (maxiter, params, nodes) = x.shape
     T = maxiter - 1
-    x_bar = np.zeros((maxiter, params, nodes))
+    x_bar = np.zeros((maxiter, params, 1))
     for t in range(maxiter):
         for i in range (params):
             x_bar[t][i] = x[t][i].mean(axis = 0)
-    
+            
     if function_type == "non convex":
         temp = [0] * T;
         for t in range(0, T):
@@ -44,10 +44,10 @@ def accuracy_check(function_type, x, oracle, x_star, f_star, mu, W, plot=True):
             temp2 = 0
             expf = 0
             f, _ = oracle(x_bar[t])
-            expf = expf + (f - f_star)
+            expf = expf + (f[0] - f_star)
             temp[t] = (t > 0)*temp[t-1]+ expf
             temp[t] = temp[t] / (t + 1)
-        
+
         if plot:
             plotError(T, temp)
         

oracle.py
@@ -9,19 +9,33 @@ class Oracle:
         self.n_nodes = n_nodes
         
         if self.type == "strongly convex":
-            self.func = lambda x: (np.power(np.linalg.norm(x), 2), 2 * x)
-            self.min = np.zeros((self.n_params, self.n_nodes))
+            
+            f = lambda col: np.power(np.linalg.norm(col, 2), 2)
+            df = lambda col: 2 * col
+            
+            self.func = lambda X: (np.apply_along_axis(f, 0, X), np.apply_along_axis(df, 0, X))
+            
+            self.x_star = np.zeros((self.n_params, 1))
+            self.f_star = 0.
         
         elif self.type == "convex":
-            a = 0.75
-            y = np.random.uniform(0, 5, (x.shape[0], 1))
             
-            self.func = lambda x: (0.5 * np.power(np.linalg.norm(x * a - y), 2), a * (x * a - y)) ## really not sure
-            self.min = y / a
+            a = 0.75
+            y = np.random.uniform(0, 5, (self.n_params, 1))
+            f = lambda col: 0.5 * np.power(np.linalg.norm(col * a - y, 2), 2)
+            df = lambda col: np.squeeze(a * (np.expand_dims(col, axis=1) * a - y), axis=1)
+            
+            self.func = lambda X: (np.apply_along_axis(f, 0, X), np.apply_along_axis(df, 0, X))
+            
+            self.x_star = y / a
+            self.f_star = 0.
         
     def getMin(self):
-        return self.min
-        
+        return self.x_star, self.f_star
+    
+    def getMu(self):
+        return -1 if (self.type != "strongly convex") else 0.5
+         
     def __call__(self, x):
         return self.func(x)
         